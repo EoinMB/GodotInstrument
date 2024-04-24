0f2ef5e7daf4f9d99e7cff8aee99148415958158
EoinMB <eoinpersonal2003@gmail.com>
2024-04-24 17:38:53 +0100

    reverts to idle animation

--- sound.gd
+++ sound.gd
@@ -4,7 +4,7 @@ var pitchV = 1
 
 func _ready():
 	$Skulla.play()
-
+	
 func _process(delta):
 			
 	$A.pitch_scale = pitchV
@@ -12,32 +12,39 @@ func _process(delta):
 	$down.pitch_scale = pitchV
 	$left.pitch_scale = pitchV
 	$right.pitch_scale = pitchV
-		
+	
+	##Notes
 	if Input.is_action_just_pressed("A_button"):
 		$A.play()
 		$Elfo.play("A")
 		$Aparticle.emitting = true
+		$Timer.start()
 		
 	if Input.is_action_just_pressed("up_button"):
 		$up.play()
 		$Elfo.play("Up")
 		$Upparticle.emitting = true
+		$Timer.start()
 		
 	if Input.is_action_just_pressed("down_button"):
 		$down.play()
 		$Elfo.play("Down")
 		$Downparticle.emitting = true
+		$Timer.start()
 		
 	if Input.is_action_just_pressed("left_button"):
 		$left.play()
 		$Elfo.play("Left")
 		$Leftparticle.emitting = true
+		$Timer.start()
 		
 	if Input.is_action_just_pressed("right_button"):
 		$right.play()
 		$Elfo.play("Right")
 		$Rightparticle.emitting = true
-
+		$Timer.start()
+	
+	##pitch
 	if Input.is_action_pressed("ResetPitch"):
 		pitchV = 1
 		
@@ -79,3 +86,7 @@ func _process(delta):
 		$DrumLeft.stop()
 		$DrumUp.stop()
 		$DrumRight.stop()
+
+##timer finish
+func _on_timer_timeout():
+	$Elfo.play("idle")
