4f046191cd93ffcd8050237af53a97b250f20e06
EoinMB <eoinpersonal2003@gmail.com>
2024-04-24 16:49:55 +0100

    flute particles

--- sound.gd
+++ sound.gd
@@ -16,22 +16,27 @@ func _process(delta):
 	if Input.is_action_just_pressed("A_button"):
 		$A.play()
 		$Elfo.play("A")
+		$Aparticle.emitting = true
 		
 	if Input.is_action_just_pressed("up_button"):
 		$up.play()
 		$Elfo.play("Up")
+		$Upparticle.emitting = true
 		
 	if Input.is_action_just_pressed("down_button"):
 		$down.play()
 		$Elfo.play("Down")
+		$Downparticle.emitting = true
 		
 	if Input.is_action_just_pressed("left_button"):
 		$left.play()
 		$Elfo.play("Left")
-	
+		$Leftparticle.emitting = true
+		
 	if Input.is_action_just_pressed("right_button"):
 		$right.play()
 		$Elfo.play("Right")
+		$Rightparticle.emitting = true
 
 	if Input.is_action_pressed("ResetPitch"):
 		pitchV = 1
@@ -53,4 +58,24 @@ func _process(delta):
 
 ##Drum Loops
 	if Input.is_action_just_pressed("DrumUp"):
+		$DrumLeft.stop()
+		$DrumUp.stop()
+		$DrumRight.stop()
 		$DrumUp.play()
+		
+	if Input.is_action_just_pressed("DrumLeft"):
+		$DrumLeft.stop()
+		$DrumUp.stop()
+		$DrumRight.stop()
+		$DrumLeft.play()
+		
+	if Input.is_action_just_pressed("DrumRight"):
+		$DrumLeft.stop()
+		$DrumUp.stop()
+		$DrumRight.stop()
+		$DrumRight.play()
+
+	if Input.is_action_just_pressed("DrumDown"):
+		$DrumLeft.stop()
+		$DrumUp.stop()
+		$DrumRight.stop()
